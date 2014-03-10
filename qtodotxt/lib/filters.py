abaf204a7af29be49950a066697b99c1450e4b3e
Ian W Scott <scottianw@gmail.com>
2014-03-10 09:50:49 -0400

    AND and OR conditions working properly (case-insensitive and unicode-friendly) in simpleTextFilter; doctests added to filters.doctest (passing); NOT condition syntax not yet implemented and NOT doctests commented out in filters.doctest

--- qtodotxt/lib/filters.py
+++ qtodotxt/lib/filters.py
@@ -1,37 +1,84 @@
 import re
 
+
 class BaseFilter(object):
+    """
+    The abstract base class for different kind of task-list filters.
+
+    """
     def __init__(self, text):
+        """
+        Initialize a new BaseFilter objects.
+
+        The required text argument (str) becomes the "text" instance attribute
+        of the object.
+
+        """
         self.text = text
+
     def isMatch(self, task):
+        """
+        Determine whether the supplied task (arg 'task') satisfies the filter.
+
+        In this base class, the test always returns True.
+
+        """
         return True
+
     def __eq__(self, other):
+        """
+        Evaluates objects as equal if their type and self.text attr are the same.
+
+        """
         if not other:
             return False
         if type(other) == type(self):
             return self.text == other.text
         return False
 
+
 class IncompleteTasksFilter(BaseFilter):
+    """
+    Task list filter that removes any completed tasks.
+
+    """
     def __init__(self):
         BaseFilter.__init__(self, 'Incomplete')
+
     def isMatch(self, task):
         return not task.is_complete
 
+
 class UncategorizedTasksFilter(BaseFilter):
+    """
+    Task list filter permitting incomplete tasks without project or context.
+
+    """
     def __init__(self):
         BaseFilter.__init__(self, 'Uncategorized')
+
     def isMatch(self, task):
         return (not task.is_complete) and (not task.contexts) and (not task.projects)
 
+
 class CompleteTasksFilter(BaseFilter):
+    """
+    Task list filter that removes any uncompleted tasks from the list.
+
+    """
     def __init__(self):
         BaseFilter.__init__(self, 'Complete')
 
     def isMatch(self, task):
         return task.is_complete
 
+
 class ContextFilter(BaseFilter):
+    """
+    Task list filter allowing only incomplete tasks with the selected context.
+
+    """
+
     def __init__(self, context):
         BaseFilter.__init__(self, context)
 
@@ -41,7 +88,12 @@ class ContextFilter(BaseFilter):
     def __str__(self):
         return "ContextFilter(%s)" % self.text
 
+
 class ProjectFilter(BaseFilter):
+    """
+    Task list filter allowing only incomplete tasks with the selected project.
+
+    """
     def __init__(self, project):
         BaseFilter.__init__(self, project)
 
@@ -51,7 +103,12 @@ class ProjectFilter(BaseFilter):
     def __str__(self):
         return "ProjectFilter(%s)" % self.text
 
+
 class HasProjectsFilter(BaseFilter):
+    """
+    Task list filter allowing only incomplete tasks with the selected project.
+
+    """
 
     def __init__(self):
         BaseFilter.__init__(self, 'Projects')
@@ -62,7 +119,12 @@ class HasProjectsFilter(BaseFilter):
     def __str__(self):
         return "HasProjectsFilter" % self.text
 
+
 class HasContextsFilter(BaseFilter):
+    """
+    Task list filter allowing only tasks tagged with some project.
+
+    """
 
     def __init__(self):
         BaseFilter.__init__(self, 'Contexts')
@@ -73,7 +135,15 @@ class HasContextsFilter(BaseFilter):
     def __str__(self):
         return "HasContextsFilter" % self.text
 
+
 class SimpleTextFilter(BaseFilter):
+    """
+    Task list filter allowing only tasks whose string matches a filter string.
+
+    This filter allows for basic and/or/not conditions in the filter string.
+    For the syntax see SimpleTextFilter.isMatch.
+
+    """
 
     def __init__(self, text):
         BaseFilter.__init__(self, text)
@@ -81,46 +151,43 @@ class SimpleTextFilter(BaseFilter):
     def isMatch(self, task):
         """
         Return a boolean based on whether the supplied task satisfies self.text.
+        TODO: the NOT syntax described below isn't yet implemented
 
         This filter can handle basic and/or/not conditions. The syntax is as
         follows:
-            - AND is indicated by a comma (,) or by a space
-            - OR is indicated by |
-            - NOT is indicated by prefixing any string with ~ or !
+
+        :AND   :   ',' or whitespace (' ')
+        :OR    :   '|'
+        :NOT   :   prefixed '~' or '!'
+
         These operators follow the following order of precedence: OR, AND, NOT.
         So, for example:
-            '@work +job1 | @home' means "Either
+
+        :'work job1 | home':                Either  (matches 'work'
+                                                     AND 'job1')
+                                            OR      (matches 'home')
+
+        :'norweigan blue ~dead | !parrot':  Either  (matches 'norweigan'
+                                                     AND 'blue'
+                                                     AND does NOT match 'dead')
+                                            OR      (does NOT match 'parrot')
 
         """
         mymatch = False
-        or_conditions = self.text.split('|')
-        or_conditions = [o for o in or_conditions if o not in ['', ' ']]
-        if or_conditions in [[], ['']]:
-            mymatch = True
-        else:
-            for or_c in or_conditions:
-                or_c = or_c.strip()
-                and_conditions = re.split(r'\s+|,\s?', or_c)
-                positives = [c.strip() for c in and_conditions
-                            if c not in ['', ' '] and c[0] not in ['~', '!']]
-                negatives = [c.strip() for c in and_conditions
-                            if c not in ['', ' '] and c[0] in ['~', '!']]
-                negmatch = any([re.search(neg, task.text, re.I) for neg in negatives]) \
-                    if negatives else False
-                posmatch = all([re.search(pos, task.text, re.I) for pos in positives]) \
-                    if positives else None
-                if posmatch and (not negmatch):
-                    mymatch = True
-                    break
+        comp = re.compile(r'\s*([\w\\-]+)[\s,]*', re.I)
+        restring = comp.sub(r'^(?=.*\1)',
+                            self.text)
+        mymatch = re.search(restring, task.text, re.I|re.U)
         return mymatch
 
     def __str__(self):
-        return "SimpleTextFilter(%s)" % self.text
+        return "SimpleTextFilter({})".format(self.text)
+
 
 class FutureFilter(BaseFilter):
 
     def __init__(self):
-        BaseFilter.__init__(self,'Future')
+        BaseFilter.__init__(self, 'Future')
 
     def isMatch(self, task):
         return not task.is_future
