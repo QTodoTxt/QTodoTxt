98d50222524e9b50e213b5e8d438bc8b5f275c95
Matthieu Nantern <matthieu.nantern@gmail.com>
2014-06-22 12:45:44 +0200

    Autocomplete list wasn't sorted anymore

--- qtodotxt/ui/services/task_editor_service.py
+++ qtodotxt/ui/services/task_editor_service.py
@@ -1,4 +1,6 @@
 from qtodotxt.ui.controls.autocomplete_inputdialog import AutoCompleteInputDialog
+from collections import OrderedDict
+
 
 
 class TaskEditorService(object):
@@ -42,7 +44,7 @@ class TaskEditorService(object):
         return text, ok
 
     def _openTaskEditor(self, title, task=None):
-        uniqlist = list(set(self._completedValues+self._values))
+        uniqlist = sorted(list(OrderedDict.fromkeys(self._completedValues+self._values)))
         dialog = AutoCompleteInputDialog(uniqlist, self._parent_window)
         dialog.setWindowTitle(title)
         dialog.setLabelText('Task:')
