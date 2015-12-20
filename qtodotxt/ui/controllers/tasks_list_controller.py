9a6404a825fb6cf618ddad3e71d9f58f3f43dbe5
olivier R-D <olivier@loke>
2015-12-20 20:38:32 +0100

    change icons to svg versions

--- qtodotxt/ui/controllers/tasks_list_controller.py
+++ qtodotxt/ui/controllers/tasks_list_controller.py
@@ -27,35 +27,35 @@ class TasksListController(QtCore.QObject):
         self._settings = settings.Settings()
 
     def _initCreateTaskAction(self):
-        action = QtGui.QAction(getIcon('add.png'), '&Create New Task', self)
+        action = QtGui.QAction(getIcon('add.svg'), '&Create New Task', self)
         action.setShortcuts(['Insert', 'Ctrl+I', 'Ctrl+N'])
         action.triggered.connect(self.createTask)
         self._view.addListAction(action)
         self.createTaskAction = action
 
     def _initDeleteSelectedTasksAction(self):
-        action = QtGui.QAction(getIcon('delete.png'), '&Delete Selected Tasks', self)
+        action = QtGui.QAction(getIcon('delete.svg'), '&Delete Selected Tasks', self)
         action.setShortcut('Delete')
         action.triggered.connect(self._deleteSelectedTasks)
         self._view.addListAction(action)
         self.deleteSelectedTasksAction = action
 
     def _initCompleteSelectedTasksAction(self):
-        action = QtGui.QAction(getIcon('x.png'), 'C&omplete Selected Tasks', self)
+        action = QtGui.QAction(getIcon('checked.svg'), 'C&omplete Selected Tasks', self)
         action.setShortcuts(['x', 'c'])
         action.triggered.connect(self._completeSelectedTasks)
         self._view.addListAction(action)
         self.completeSelectedTasksAction = action
 
     def _initDecreasePrioritySelectedTasksAction(self):
-        action = QtGui.QAction(getIcon('decrease.png'), 'Decrease priority', self)
+        action = QtGui.QAction(getIcon('decrease.svg'), 'Decrease priority', self)
         action.setShortcuts(['-', '<'])
         action.triggered.connect(self._decreasePriority)
         self._view.addListAction(action)
         self.decreasePrioritySelectedTasksAction = action
 
     def _initIncreasePrioritySelectedTasksAction(self):
-        action = QtGui.QAction(getIcon('increase.png'), 'Increase priority', self)
+        action = QtGui.QAction(getIcon('increase.svg'), 'Increase priority', self)
         action.setShortcuts(['+', '>'])
         action.triggered.connect(self._increasePriority)
         self._view.addListAction(action)
