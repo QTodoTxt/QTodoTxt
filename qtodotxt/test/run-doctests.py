2223af82993bcce20642eb64a06ca4b85cb0e2a1
kmicc <kmic2006@gmx.net>
2016-02-11 00:12:50 +0100

    rewrite run-doctests.py (remove unittests) (taken from #156, original author: funkyfuture)

--- qtodotxt/test/run-doctests.py
+++ qtodotxt/test/run-doctests.py
@@ -1,62 +1,40 @@
 #!/usr/bin/env python3
 
-import sys
-import os
-import unittest
 import doctest
+import os
+from os import path
+import sys
 
-testsdir = os.path.abspath(os.path.dirname(__file__))
-root = os.path.join(testsdir, '..', '..')
-sys.path.insert(0, os.path.abspath(root))
+try:
+    import PySide  # noqa
+except ImportError:
+    print("Couldn't import PySide. Aborting. These are the PYTHONPATHs:")
+    print(sys.path)
+    sys.exit(1)
 
-tests = unittest.defaultTestLoader.discover('.', pattern='test*.py')
+
+testsdir = path.abspath(path.dirname(__file__))
+root = path.abspath(path.join(testsdir, '..', '..'))
+sys.path.insert(0, root)
 
 
 def run_doctests(testsdir):
     exit_code = 0
     for filename in os.listdir(testsdir):
-        fullname = os.path.join(testsdir, filename)
+        fullname = path.join(testsdir, filename)
         if filename.endswith('.doctest'):
             print("- Running", fullname)
             result = doctest.testfile(fullname, module_relative=False)
             print("  => ran {0} results, {1} failed".format(result.attempted, result.failed))
             exit_code += result.failed
-        elif os.path.isdir(fullname):
+        elif path.isdir(fullname):
             exit_code += run_doctests(fullname)
 
     return exit_code
 
 if __name__ == "__main__":
-    try:
-        from flake8 import main as flake8
-        sys.argv.append(os.path.dirname(__file__))
-        flake8.main()
-    except (ImportError, SystemExit):
-        pass
-
     exit_code = 0
 
-    print("========================================")
-    print("Running Unittests")
-    print("========================================")
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if not result.wasSuccessful():
-        exit_code = 1
-
-    print("========================================")
-    print("Running Doctests")
-    print("========================================")
-    doctests_exit_code = run_doctests(testsdir)
-    if exit_code == 0:
-        exit_code = doctests_exit_code
-
-    if exit_code == 0:
-        print("========================================")
-        print("Unit tests passed successfuly")
-        print("========================================")
-    else:
-        print("========================================")
-        print("Unit tests failed")
-        print("========================================")
-
+    print("Running Doctests...")
+    exit_code = run_doctests(testsdir)
     sys.exit(exit_code)
