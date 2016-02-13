78cffd18e7a4071bab6c9bc2bf327bc3d84337ab
kmicc <kmic2006@gmx.net>
2016-02-13 19:02:18 +0100

    use new icons

--- qtodotxt/ui/controllers/menu_controller.py
+++ qtodotxt/ui/controllers/menu_controller.py
@@ -71,7 +71,7 @@ class MenuController(QtCore.QObject):
         return action
 
     def _createSaveAction(self):
-        action = QtGui.QAction(getIcon('FileSave.svg'), '&Save', self)
+        action = QtGui.QAction(getIcon('FileSave.png'), '&Save', self)
         action.setShortcuts(["Ctrl+S"])
         action.triggered.connect(self._main_controller.save)
         self.saveAction = action
