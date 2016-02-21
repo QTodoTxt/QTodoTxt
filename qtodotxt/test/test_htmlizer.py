e5937ec793dacab70b30c76880f5c413fea51997
kmicc <kmic2006@gmx.net>
2016-02-21 04:52:25 +0100

    fix tests

--- qtodotxt/test/test_htmlizer.py
+++ qtodotxt/test/test_htmlizer.py
@@ -11,39 +11,39 @@ class TestHtmlizer(unittest.TestCase):
         # Simple task should return simple html
         task = tasklib.Task('this is my task')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>this is my task')
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;this is my task')
 
     def test_02(self):
         # Test task with a single context at the end
         task = tasklib.Task('this is my task @context')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>this is my task <font color="green">@context</font>')
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;this is my task <font color="green">@context</font>')
 
     def test_03(self):
         # Test task with a single context at the center
         task = tasklib.Task('this is my task @context and some more words')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>this is my task <font color="green">@context</font> and some more '
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;this is my task <font color="green">@context</font> and some more '
                          'words')
 
     def test_04(self):
         # Test task with a single project at the end
         task = tasklib.Task('this is my task +project')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>this is my task <font style="color:#64AAD0">+project</font>')
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;this is my task <font style="color:#64AAD0">+project</font>')
 
     def test_05(self):
         # Test task with a single project at the center
         task = tasklib.Task('this is my task +project and some more words')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>this is my task <font style="color:#64AAD0">+project</font> and '
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;this is my task <font style="color:#64AAD0">+project</font> and '
                          'some more words')
 
     def test_06(self):
         # Test task with a single context and a single project
         task = tasklib.Task('this is my task @context and +project and some more words')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>this is my task <font color="green">@context</font> and '
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;this is my task <font color="green">@context</font> and '
                          '<font style="color:#64AAD0">+project</font> and some more words')
 
     def test_07(self):
@@ -52,7 +52,7 @@ class TestHtmlizer(unittest.TestCase):
                             '+project1 +project2 and +project3 some more '
                             'words')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>this is my task <font color="green">@context1</font> and '
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;this is my task <font color="green">@context1</font> and '
                          '<font color="green">@context2</font> and <font style="color:#64AAD0">+project1</font> '
                          '<font style="color:#64AAD0">+project2</font> and <font style="color:#64AAD0">+project3</font>'
                          ' some more words')
@@ -85,20 +85,20 @@ class TestHtmlizer(unittest.TestCase):
         # Test task with an invalid due date
         task = tasklib.Task('this is my task due:2014-04')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>this is my task <b><font style="color:red">*** Invalid date format, expected: YYYY-mm-dd! due:2014-04s ***</font></b>')
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;this is my task <b><font style="color:red">*** Invalid date format, expected: YYYY-mm-dd! due:2014-04s ***</font></b>')
 
     def test_13(self):
         # Test task with an invalid Threshold date
         task = tasklib.Task('this is my task t:2014-04')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>this is my task <b><font style="color:red">*** Invalid date '
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;this is my task <b><font style="color:red">*** Invalid date '
                          'format, expected: YYYY-mm-dd! t:2014-04 ***</font></b>')
 
     def test_14(self):
         # Test task with an URL
         task = tasklib.Task('Download https://github.com/mNantern/QTodoTxt/archive/master.zip and extract')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt>Download '
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;Download '
                          '<a href="https://github.com/mNantern/QTodoTxt/archive/master.zip">'
                          'https://github.com/mNantern/QTodoTxt/archive/master.zip</a> and extract')
 
@@ -106,5 +106,5 @@ class TestHtmlizer(unittest.TestCase):
         # Test task with solely an URL
         task = tasklib.Task('https://github.com/mNantern/QTodoTxt/archive/master.zip')
         self.assertEqual(self.htmlizer.task2html(task),
-                         '<tt>&nbsp;&nbsp;&nbsp;</tt><a href="https://github.com/mNantern/QTodoTxt/archive/master.zip">'
+                         '<tt>&nbsp;&nbsp;&nbsp;</tt>&nbsp;<a href="https://github.com/mNantern/QTodoTxt/archive/master.zip">'
                          'https://github.com/mNantern/QTodoTxt/archive/master.zip</a>')
