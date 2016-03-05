ff975013218a5e84a1d7052a616b2cdb3c54b2f0
olivier R-D <olivier@loke>
2016-03-05 14:44:59 +0100

    fix date completion

--- qtodotxt/ui/dialogs/taskeditor_dialog.py
+++ qtodotxt/ui/dialogs/taskeditor_dialog.py
@@ -10,7 +10,9 @@ class TaskEditorDialog(QtWidgets.QDialog):
         ('due:Today', ''),
         ('due:Tomorrow', ''),
         ('due:EndOfWeek', ''),
+        ('due:EndOfNextWeek', ''),
         ('due:EndOfMonth', ''),
+        ('due:EndOfNextMonth', ''),
         ('due:EndOfYear', ''),
         ('due:January', ''),
         ('due:February', ''),
@@ -32,40 +34,45 @@ class TaskEditorDialog(QtWidgets.QDialog):
         self._populateKeys(self.autocomplete_pairs)
 
     def _endOfMonth(self, month):
-        month %= 12
-
-        eom = date.today().replace(month=month + 1, day=1) - timedelta(days=1)
-        if eom < date.today():
-            eom = eom.replace(year=eom.year + 1)
-        return 'due:' + eom.strftime('%Y-%m-%d')
+        today = date.today()
+        month = month + 1
+        year = today.year
+        if month > 12:
+            month = month - 12
+            year += 1
+        if month <= today.month:
+            year += 1
+        eom = date(year=year, month=month, day=1) - timedelta(days=1)
+        return eom.isoformat()
 
     def _populateKeys(self, keys):
-        today = 'due:' + date.today().strftime('%Y-%m-%d')
-        tomorrow = 'due:' + (date.today() + timedelta(days=1)).strftime('%Y-%m-%d')
-        EOW = 'due:' + (date.today() + timedelta((6 - date.today().weekday()) % 7)).strftime('%Y-%m-%d')
-        EOM = 'due:' + ((date.today().replace(day=1) +
-                        timedelta(days=32)).replace(day=1) -
-                        timedelta(days=1)).strftime('%Y-%m-%d')
-        EOY = 'due:' + (date.today().replace(year=date.today().year + 1, month=1, day=1) -
-                        timedelta(days=1)).strftime('%Y-%m-%d')
+        today = 'due:' + date.today().isoformat()
+        tomorrow = 'due:' + (date.today() + timedelta(days=1)).isoformat()
+        EOW = 'due:' + (date.today() + timedelta((6 - date.today().weekday()) % 7)).isoformat()
+        EONW = 'due:' + (date.today() + timedelta((13 - date.today().weekday()) % 14)).isoformat()
+        EOM = 'due:' + self._endOfMonth(date.today().month)
+        EONM = 'due:' + self._endOfMonth(date.today().month + 1)
+        EOY = 'due:' + (date(year=date.today().year + 1, month=1, day=1) - timedelta(days=1)).isoformat()
 
         keys['due:EndOfWeek'] = EOW
+        keys['due:EndOfNextWeek'] = EONW
         keys['due:EndOfMonth'] = EOM
+        keys['due:EndOfNextMonth'] = EONM
         keys['due:EndOfYear'] = EOY
         keys['due:Today'] = today
         keys['due:Tomorrow'] = tomorrow
-        # keys['due:January'] = self._endOfMonth(1)
-        # keys['due:February'] = self._endOfMonth(2)
-        # keys['due:March'] = self._endOfMonth(3)
-        # keys['due:April'] = self._endOfMonth(4)
-        # keys['due:May'] = self._endOfMonth(5)
-        # keys['due:June'] = self._endOfMonth(6)
-        # keys['due:July'] = self._endOfMonth(7)
-        # keys['due:August'] = self._endOfMonth(8)
-        # keys['due:September'] = self._endOfMonth(9)
-        # keys['due:October'] = self._endOfMonth(10)
-        # keys['due:November'] = self._endOfMonth(11)
-        # keys['due:December'] = self._endOfMonth(12)
+        keys['due:January'] = "due:" + self._endOfMonth(1)
+        keys['due:February'] = "due:" + self._endOfMonth(2)
+        keys['due:March'] = "due:" + self._endOfMonth(3)
+        keys['due:April'] = "due:" + self._endOfMonth(4)
+        keys['due:May'] = "due:" + self._endOfMonth(5)
+        keys['due:June'] = "due:" + self._endOfMonth(6)
+        keys['due:July'] = "due:" + self._endOfMonth(7)
+        keys['due:August'] = "due:" + self._endOfMonth(8)
+        keys['due:September'] = "due:" + self._endOfMonth(9)
+        keys['due:October'] = "due:" + self._endOfMonth(10)
+        keys['due:November'] = "due:" + self._endOfMonth(11)
+        keys['due:December'] = "due:" + self._endOfMonth(12)
         return keys
 
     def _initUI(self, values):
