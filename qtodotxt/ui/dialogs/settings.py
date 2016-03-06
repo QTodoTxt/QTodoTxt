625354fc67ec9a2360910b4fe21bced6b32906d6
olivier R-D <olivier@loke>
2016-03-06 10:26:51 +0100

    add option to enable or disable system tray

--- qtodotxt/ui/dialogs/settings.py
+++ qtodotxt/ui/dialogs/settings.py
@@ -23,6 +23,7 @@ class Settings(QtWidgets.QDialog):
         self._int_settings_to_cb("add_created_date", self.ui.addCreatedDateCheckBox)
         self._int_settings_to_cb("hide_future_tasks", self.ui.hideFutureTasksCheckBox)
         self._int_settings_to_cb("confirm_complete", self.ui.confirmCompletionCheckBox)
+        self._int_settings_to_cb("enable_tray", self.ui.trayCheckBox)
         priority = self.settings.value("lowest_priority", "D")
         self.ui.lowestPriorityLineEdit.setText(priority)
 
@@ -40,6 +41,7 @@ class Settings(QtWidgets.QDialog):
         self.ui.addCreatedDateCheckBox.stateChanged.connect(self.setAddCreatedDate)
         self.ui.hideFutureTasksCheckBox.stateChanged.connect(self.setHideFutureTasks)
         self.ui.confirmCompletionCheckBox.stateChanged.connect(self.setConfirmCompletion)
+        self.ui.trayCheckBox.stateChanged.connect(self.enableTray)
         self.ui.lowestPriorityLineEdit.textChanged.connect(self.setLowestPriority)
 
     def _save_int_cb(self, name, val):
@@ -63,6 +65,9 @@ class Settings(QtWidgets.QDialog):
 
     def setConfirmCompletion(self, val):
         self._save_int_cb("comfirm_complete", val)
+
+    def enableTray(self, val):
+        self._save_int_cb("enable_tray", val)
 
     def setLowestPriority(self, text):
         self.settings.setValue("lowest_priority", text)
