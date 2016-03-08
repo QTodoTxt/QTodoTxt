512ea8f355f8907b68bd83408b35c061896d2bae
olivier R-D <olivier@loke>
2016-03-08 19:03:17 +0100

    bump dev version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 
 setup(name="qtodotxt", 
-      version="1.5.0",
+      version="1.6.0alpha2",
       description="Cross Platform todo.txt GUI",
       author="QTT Development Team",
       author_email="qtodotxt@googlegroups.com",
