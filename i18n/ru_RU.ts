e6b30f2ab0a9e56c55ad2c4ab7a082479f630a04
PKEv <PKEv@yandex.ru>
2016-11-03 00:53:07 +0300

    ru_RU i18n

--- i18n/ru_RU.ts
+++ i18n/ru_RU.ts
@@ -1,21 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="2.0" language="ru_RU" sourcelanguage="en">
 <context>
+    <name>MainController</name>
+    <message>
+        <location filename="main_controller.py" line="375"/>
+        <source>Revert to saved file (and lose unsaved changes)?</source>
+        <translation type="unfinished">Закрыть и отказаться от изменений?</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsUI</name>
+    <message>
+        <location filename="settingsui.py" line="89"/>
+        <source>Preferences</source>
+        <translation type="unfinished">Настройки</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="90"/>
+        <source>QTodoTxt Settings</source>
+        <translation type="unfinished">QTodoTxt Настройки</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="91"/>
+        <source>Autosave</source>
+        <translation type="unfinished">Автосохранение</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="92"/>
+        <source>AutoArchive</source>
+        <translation type="unfinished">АвтоАрхивирование</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="93"/>
+        <source>Add created date</source>
+        <translation type="unfinished">Добавлять дату создания</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="94"/>
+        <source>Ask for confirmation before task completion</source>
+        <translation type="unfinished">Спрашивать подтверждение при завершении задачи</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="95"/>
+        <source>Lowest task priority</source>
+        <translation type="unfinished">Наименьший приоритет задачи</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="96"/>
+        <source>Close</source>
+        <translation type="unfinished">Закрыть</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="98"/>
+        <source>Show Delete action (Requires restart)</source>
+        <translation type="unfinished">Разрешить удаление задач (Требуется перезапуск)</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="99"/>
+        <source>Enable system tray (Requires restart)</source>
+        <translation type="unfinished">Показывать значек в трее (Требуется перезапуск)</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="100"/>
+        <source>Show/Hide on Tray Click</source>
+        <translation type="unfinished">Отображать/Скрывать по щелчку в трее</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="101"/>
+        <source>Hide on Startup</source>
+        <translation type="unfinished">Скрывать при запуске приложения</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="102"/>
+        <source>Close to Tray</source>
+        <translation type="unfinished">Сворачивать в трей при закрытии</translation>
+    </message>
+</context>
+<context>
     <name>TrayIcon</name>
     <message>
-        <location filename="app.py" line="32"/>
+        <location filename="app.py" line="31"/>
         <source>Create New Task</source>
         <translation type="unfinished">Создать новую задачу</translation>
     </message>
     <message>
-        <location filename="app.py" line="34"/>
+        <location filename="app.py" line="33"/>
         <source>Show/Hide Window</source>
-        <translation type="unfinished">Показать/Спрятать окно</translation>
+        <translation type="unfinished">Отобразить/Скрыть</translation>
     </message>
     <message>
-        <location filename="app.py" line="36"/>
+        <location filename="app.py" line="35"/>
         <source>Exit</source>
-        <translation type="unfinished">Закрыть</translation>
+        <translation type="unfinished">Выход</translation>
     </message>
 </context>
 </TS>
