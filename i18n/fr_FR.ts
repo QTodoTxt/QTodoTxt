edf86256a5f27a124514c4025e7f6b0ff9186012
PKEv <PKEv@yandex.ru>
2016-11-21 00:04:42 +0300

    add link to weblate
    add link
    add more langs
    add icon in readme

---
+++ i18n/fr_FR.ts
@@ -0,0 +1,311 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>MainController</name>
+    <message>
+        <location filename="main_controller.py" line="70"/>
+        <source>Show &amp;Filters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="76"/>
+        <source>Show Future &amp;Tasks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="82"/>
+        <source>Show &amp;Completed Tasks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="88"/>
+        <source>&amp;Archive Completed Tasks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="93"/>
+        <source>Show Tool&amp;Bar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="98"/>
+        <source>Show Search Bar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="325"/>
+        <source>Unsaved changes...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="399"/>
+        <source>Revert to saved file (and lose unsaved changes)?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="413"/>
+        <source>Current file &apos;{}&apos; is not available.
+Exception: {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="416"/>
+        <source>Error opening file: {}.
+ Exception:{}</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MenuController</name>
+    <message>
+        <location filename="menu_controller.py" line="26"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="67"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="82"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="91"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="103"/>
+        <source>&amp;Shortcuts list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="119"/>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="128"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="136"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="144"/>
+        <source>&amp;Revert</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="151"/>
+        <source>&amp;Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="menu_controller.py" line="159"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsUI</name>
+    <message>
+        <location filename="settingsui.py" line="100"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="101"/>
+        <source>QTodoTxt Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="102"/>
+        <source>Autosave</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="103"/>
+        <source>Single instance (need restart)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="104"/>
+        <source>AutoArchive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="105"/>
+        <source>Add created date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="106"/>
+        <source>Ask for confirmation before task completion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="107"/>
+        <source>Lowest task priority</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="108"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="110"/>
+        <source>Show Delete action (Requires restart)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="111"/>
+        <source>Enable system tray (Requires restart)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="112"/>
+        <source>Show/Hide on Tray Click</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="113"/>
+        <source>Hide on Startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="114"/>
+        <source>Close to Tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="115"/>
+        <source>Color schem (Requires restart):</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Shortcuts</name>
+    <message>
+        <location filename="shortcuts.py" line="24"/>
+        <source>Comment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shortcuts.py" line="25"/>
+        <source>Shortcut</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ShortcutsUI</name>
+    <message>
+        <location filename="shortcutsui.py" line="47"/>
+        <source>Shortcuts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shortcutsui.py" line="48"/>
+        <source>Shortcuts list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="shortcutsui.py" line="49"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TasksListController</name>
+    <message>
+        <location filename="tasks_list_controller.py" line="42"/>
+        <source>&amp;Edit Task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="51"/>
+        <source>&amp;Create New Task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="59"/>
+        <source>&amp;Create a new Task based on a template</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="67"/>
+        <source>Copy Selected Tasks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="75"/>
+        <source>&amp;Delete Selected Tasks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="83"/>
+        <source>C&amp;omplete Selected Tasks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="91"/>
+        <source>Decrease Priority</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="99"/>
+        <source>Increase Priority</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="120"/>
+        <source>Toggle Completeness of</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="127"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="134"/>
+        <source>&lt;b&gt;%s the following task?&lt;/b&gt;&lt;ul&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="136"/>
+        <source>&lt;b&gt;%s the following tasks?&lt;/b&gt;&lt;ul&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="140"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="app.py" line="42"/>
+        <source>Create New Task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="app.py" line="45"/>
+        <source>Show/Hide Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="app.py" line="47"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
