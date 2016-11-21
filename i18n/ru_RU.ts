d652bfc4c6750c7d6b95240dfc0698e487b6dfa0
PKEv <PKEv@yandex.ru>
2016-11-21 16:22:38 +0300

    update ru

--- i18n/ru_RU.ts
+++ i18n/ru_RU.ts
@@ -1,59 +1,60 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="ru_RU" sourcelanguage="en">
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU" sourcelanguage="en">
 <context>
     <name>MainController</name>
     <message>
         <location filename="main_controller.py" line="392"/>
         <source>Revert to saved file (and lose unsaved changes)?</source>
-        <translation type="unfinished">Закрыть и отказаться от изменений?</translation>
+        <translation>Закрыть и отказаться от изменений?</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="71"/>
         <source>Show &amp;Filters</source>
-        <translation type="unfinished">Показывать &amp;фильтры</translation>
+        <translation>Показывать &amp;фильтры</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="76"/>
         <source>Show Future &amp;Tasks</source>
-        <translation type="unfinished">Показывать &amp;будующие задачи</translation>
+        <translation>Показывать &amp;будущие задачи</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="81"/>
         <source>Show &amp;Completed Tasks</source>
-        <translation type="unfinished">Показывать &amp;выполненые задачи</translation>
+        <translation>Показывать &amp;выполненные задачи</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="87"/>
         <source>&amp;Archive Completed Tasks</source>
-        <translation type="unfinished">&amp;Архивировать выполненые задачи</translation>
+        <translation>&amp;Архивировать выполненные задачи</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="91"/>
         <source>Show Tool&amp;Bar</source>
-        <translation type="unfinished">Показать панель</translation>
+        <translation>Показать &amp;панель</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="318"/>
         <source>Unsaved changes...</source>
-        <translation type="unfinished">Несохраненные изменения...</translation>
+        <translation>Не сохраненные изменения...</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="95"/>
         <source>Show Search Bar</source>
-        <translation type="unfinished">Показать строку поиска</translation>
+        <translation>Показать строку поиска</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="406"/>
-        <source>Current file &apos;{}&apos; is not available.
+        <source>Current file '{}' is not available.
 Exception: {}</source>
-        <translation type="unfinished">Текущий файл &apos;{}&apos; не доступен.
+        <translation>Текущий файл '{}' не доступен.
 Ошибка: {}</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="409"/>
         <source>Error opening file: {}.
  Exception:{}</source>
-        <translation type="unfinished">Ошибка открытия файла: {}.
+        <translation>Ошибка открытия файла: {}.
  Ошибка:{}</translation>
     </message>
 </context>
@@ -62,52 +63,52 @@ Exception: {}</source>
     <message>
         <location filename="menu_controller.py" line="26"/>
         <source>&amp;File</source>
-        <translation type="unfinished">&amp;Файл</translation>
+        <translation>&amp;Файл</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="65"/>
         <source>&amp;Edit</source>
-        <translation type="unfinished">&amp;Правка</translation>
+        <translation>&amp;Правка</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="79"/>
         <source>&amp;View</source>
-        <translation type="unfinished">&amp;Вид</translation>
+        <translation>&amp;Вид</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="88"/>
         <source>&amp;Help</source>
-        <translation type="unfinished">&amp;Справка</translation>
+        <translation>&amp;Справка</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="100"/>
         <source>&amp;New</source>
-        <translation type="unfinished">&amp;Новый</translation>
+        <translation>&amp;Новый</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="108"/>
         <source>&amp;Open</source>
-        <translation type="unfinished">&amp;Открыть</translation>
+        <translation>&amp;Открыть</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="115"/>
         <source>&amp;Save</source>
-        <translation type="unfinished">&amp;Сохранить</translation>
+        <translation>&amp;Сохранить</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="122"/>
         <source>&amp;Revert</source>
-        <translation type="unfinished">&amp;Перечитать</translation>
+        <translation>&amp;Перечитать</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="128"/>
         <source>&amp;Preferences</source>
-        <translation type="unfinished">&amp;Настройки</translation>
+        <translation>&amp;Настройки</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="134"/>
         <source>E&amp;xit</source>
-        <translation type="unfinished">&amp;Выход</translation>
+        <translation>&amp;Выход</translation>
     </message>
 </context>
 <context>
@@ -115,77 +116,77 @@ Exception: {}</source>
     <message>
         <location filename="settingsui.py" line="100"/>
         <source>Preferences</source>
-        <translation type="unfinished">Настройки</translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="101"/>
         <source>QTodoTxt Settings</source>
-        <translation type="unfinished">QTodoTxt Настройки</translation>
+        <translation>QTodoTxt Настройки</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="102"/>
         <source>Autosave</source>
-        <translation type="unfinished">Автосохранение</translation>
+        <translation>АвтоCохранение</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="104"/>
         <source>AutoArchive</source>
-        <translation type="unfinished">АвтоАрхивирование</translation>
+        <translation>АвтоАрхивирование</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="105"/>
         <source>Add created date</source>
-        <translation type="unfinished">Добавлять дату создания</translation>
+        <translation>Добавлять дату создания</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="106"/>
         <source>Ask for confirmation before task completion</source>
-        <translation type="unfinished">Спрашивать подтверждение при завершении задачи</translation>
+        <translation>Спрашивать подтверждение при завершении задачи</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="107"/>
         <source>Lowest task priority</source>
-        <translation type="unfinished">Наименьший приоритет задачи</translation>
+        <translation>Наименьший приоритет задачи</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="108"/>
         <source>Close</source>
-        <translation type="unfinished">Закрыть</translation>
+        <translation>Закрыть</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="110"/>
         <source>Show Delete action (Requires restart)</source>
-        <translation type="unfinished">Разрешить удаление задач (Требуется перезапуск)</translation>
+        <translation>Разрешить удаление задач (Требуется перезапуск)</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="111"/>
         <source>Enable system tray (Requires restart)</source>
-        <translation type="unfinished">Показывать значек в трее (Требуется перезапуск)</translation>
+        <translation>Показывать значёк в трее (Требуется перезапуск)</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="112"/>
         <source>Show/Hide on Tray Click</source>
-        <translation type="unfinished">Отображать/Скрывать по щелчку в трее</translation>
+        <translation>Отображать/Скрывать по щелчку в трее</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="113"/>
         <source>Hide on Startup</source>
-        <translation type="unfinished">Скрывать при запуске приложения</translation>
+        <translation>Скрывать при запуске приложения</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="114"/>
         <source>Close to Tray</source>
-        <translation type="unfinished">Сворачивать в трей при закрытии</translation>
+        <translation>Сворачивать в трей при закрытии</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="103"/>
         <source>Single instanse (need restart)</source>
-        <translation type="unfinished">Одноэкземплярное приложение (требуется перезапуск)</translation>
+        <translation>Одноэкземплярное приложение (требуется перезапуск)</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="115"/>
         <source>Color schem (Requires restart):</source>
-        <translation type="unfinished">Цветовая схема (требуется перезапуск):</translation>
+        <translation>Цветовая схема (требуется перезапуск):</translation>
     </message>
 </context>
 <context>
@@ -193,62 +194,62 @@ Exception: {}</source>
     <message>
         <location filename="tasks_list_controller.py" line="37"/>
         <source>&amp;Edit Task</source>
-        <translation type="unfinished">&amp;Редактировать задачу</translation>
+        <translation>&amp;Редактировать задачу</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="44"/>
         <source>&amp;Create New Task</source>
-        <translation type="unfinished">&amp;Создать новую задачу</translation>
+        <translation>&amp;Создать новую задачу</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="51"/>
         <source>Copy Selected Tasks</source>
-        <translation type="unfinished">&amp;Копировать выделенные задачи</translation>
+        <translation>&amp;Копировать выделенные задачи</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="58"/>
         <source>&amp;Delete Selected Tasks</source>
-        <translation type="unfinished">&amp;Удалить выделенные задачи</translation>
+        <translation>&amp;Удалить выделенные задачи</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="65"/>
         <source>C&amp;omplete Selected Tasks</source>
-        <translation type="unfinished">Отметить выделенную задачу как выполненую</translation>
+        <translation>Отметить выделенную задачу как выполненную</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="72"/>
         <source>Decrease Priority</source>
-        <translation type="unfinished">Увеличить приоритет</translation>
+        <translation>Уменьшить приоритет</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="79"/>
         <source>Increase Priority</source>
-        <translation type="unfinished">Понизить приоритет</translation>
+        <translation>Увеличить приоритет</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="99"/>
         <source>Toggle Completeness of</source>
-        <translation type="unfinished">Подтвердить выолнение задач</translation>
+        <translation>Подтвердить выполнение задач</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="106"/>
         <source>Delete</source>
-        <translation type="unfinished">Удаление</translation>
+        <translation>Удаление</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="113"/>
         <source>&lt;b&gt;%s the following task?&lt;/b&gt;&lt;ul&gt;</source>
-        <translation type="unfinished">&lt;b&gt;%s следующих задач?&lt;/b&gt;&lt;ul&gt;</translation>
+        <translation>&lt;b&gt;%s следующих задач?&lt;/b&gt;&lt;ul&gt;</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="115"/>
         <source>&lt;b&gt;%s the following tasks?&lt;/b&gt;&lt;ul&gt;</source>
-        <translation type="unfinished">&lt;b&gt;%s следующих задач?&lt;/b&gt;&lt;ul&gt;</translation>
+        <translation>&lt;b&gt;%s следующих задач?&lt;/b&gt;&lt;ul&gt;</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="119"/>
         <source>Confirm</source>
-        <translation type="unfinished">Подтверждение</translation>
+        <translation>Подтверждение</translation>
     </message>
 </context>
 <context>
@@ -256,17 +257,17 @@ Exception: {}</source>
     <message>
         <location filename="app.py" line="37"/>
         <source>Create New Task</source>
-        <translation type="unfinished">Создать новую задачу</translation>
+        <translation>Создать новую задачу</translation>
     </message>
     <message>
         <location filename="app.py" line="39"/>
         <source>Show/Hide Window</source>
-        <translation type="unfinished">Отобразить/Скрыть</translation>
+        <translation>Отобразить/Скрыть</translation>
     </message>
     <message>
         <location filename="app.py" line="41"/>
         <source>Exit</source>
-        <translation type="unfinished">Выход</translation>
+        <translation>Выход</translation>
     </message>
 </context>
 </TS>
