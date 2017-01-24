35e7d736f66b8f05e86a3192aa10bfdab6dfca66
glenn <dev@dev-machine>
2017-01-24 15:26:53 +0100

    Add French translation

--- i18n/fr_FR.ts
+++ i18n/fr_FR.ts
@@ -8,48 +8,48 @@
         <translation>Montrer les filtres</translation>
     </message>
     <message>
-        <location filename="main_controller.py" line="76"/>
-        <source>Show Future &amp;Tasks</source>
-        <translation>Montrer les tâches à venir</translation>
-    </message>
-    <message>
-        <location filename="main_controller.py" line="82"/>
-        <source>Show &amp;Completed Tasks</source>
-        <translation>Montrer les tâches terminées</translation>
-    </message>
-    <message>
-        <location filename="main_controller.py" line="88"/>
-        <source>&amp;Archive Completed Tasks</source>
-        <translation>Archiver les tâches complétées</translation>
-    </message>
-    <message>
-        <location filename="main_controller.py" line="93"/>
-        <source>Show Tool&amp;Bar</source>
-        <translation>Montrer la barre d'outils</translation>
-    </message>
-    <message>
-        <location filename="main_controller.py" line="98"/>
-        <source>Show Search Bar</source>
-        <translation>Montrer la barre de recherche</translation>
-    </message>
-    <message>
-        <location filename="main_controller.py" line="325"/>
+        <location filename="main_controller.py" line="324"/>
         <source>Unsaved changes...</source>
         <translation>Changements non sauvegardés...</translation>
     </message>
     <message>
-        <location filename="main_controller.py" line="399"/>
+        <location filename="main_controller.py" line="398"/>
         <source>Revert to saved file (and lose unsaved changes)?</source>
         <translation>Revenir au fichier anterieur (et perdre les changements non sauvegardés)?</translation>
     </message>
     <message>
-        <location filename="main_controller.py" line="413"/>
+        <location filename="main_controller.py" line="76"/>
+        <source>Show future &amp;Tasks</source>
+        <translation>Afficher les tâches à venir</translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="82"/>
+        <source>Show &amp;Completed tasks</source>
+        <translation>Afficher les tâches terminées</translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="88"/>
+        <source>&amp;Archive completed tasks</source>
+        <translation>Archiver les tâches terminées</translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="93"/>
+        <source>Show tool&amp;Bar</source>
+        <translation>Afficher la barre d&apos;outil</translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="98"/>
+        <source>Show search bar</source>
+        <translation>Afficher la barre de recherche</translation>
+    </message>
+    <message>
+        <location filename="main_controller.py" line="412"/>
         <source>Current file &apos;{}&apos; is not available.
 Exception: {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main_controller.py" line="416"/>
+        <location filename="main_controller.py" line="415"/>
         <source>Error opening file: {}.
  Exception:{}</source>
         <translation type="unfinished"></translation>
@@ -98,11 +98,6 @@ Exception: {}</source>
         <translation>Sauver</translation>
     </message>
     <message>
-        <location filename="menu_controller.py" line="144"/>
-        <source>&amp;Revert</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="menu_controller.py" line="151"/>
         <source>&amp;Preferences</source>
         <translation>Préferences</translation>
@@ -112,96 +107,126 @@ Exception: {}</source>
         <source>E&amp;xit</source>
         <translation>Quitter</translation>
     </message>
+    <message>
+        <location filename="menu_controller.py" line="144"/>
+        <source>&amp;Revert</source>
+        <translation>Retablir</translation>
+    </message>
 </context>
 <context>
     <name>SettingsUI</name>
     <message>
-        <location filename="settingsui.py" line="100"/>
-        <source>Preferences</source>
-        <translation>Préferences</translation>
-    </message>
-    <message>
-        <location filename="settingsui.py" line="101"/>
+        <location filename="settingsui.py" line="107"/>
         <source>QTodoTxt Settings</source>
         <translation>Préferences QTodoTxt</translation>
     </message>
     <message>
-        <location filename="settingsui.py" line="102"/>
+        <location filename="settingsui.py" line="108"/>
         <source>Autosave</source>
         <translation>Sauvegarde automatique</translation>
     </message>
     <message>
-        <location filename="settingsui.py" line="103"/>
-        <source>Single instance (need restart)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="settingsui.py" line="104"/>
+        <location filename="settingsui.py" line="109"/>
         <source>AutoArchive</source>
         <translation>Archivage automatique</translation>
     </message>
     <message>
-        <location filename="settingsui.py" line="105"/>
+        <location filename="settingsui.py" line="110"/>
         <source>Add created date</source>
         <translation>Ajouter date de création</translation>
     </message>
     <message>
-        <location filename="settingsui.py" line="106"/>
+        <location filename="settingsui.py" line="111"/>
         <source>Ask for confirmation before task completion</source>
         <translation>Demander confirmation avant de terminer la tâche</translation>
     </message>
     <message>
-        <location filename="settingsui.py" line="107"/>
+        <location filename="settingsui.py" line="113"/>
         <source>Lowest task priority</source>
         <translation>Diminuer la priorité de la tâche</translation>
     </message>
     <message>
-        <location filename="settingsui.py" line="108"/>
+        <location filename="settingsui.py" line="120"/>
         <source>Close</source>
         <translation>Fermer</translation>
     </message>
     <message>
-        <location filename="settingsui.py" line="110"/>
-        <source>Show Delete action (Requires restart)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="settingsui.py" line="111"/>
-        <source>Enable system tray (Requires restart)</source>
+        <location filename="settingsui.py" line="106"/>
+        <source>SettingsUI</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="settingsui.py" line="112"/>
-        <source>Show/Hide on Tray Click</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="settingsui.py" line="113"/>
-        <source>Hide on Startup</source>
-        <translation type="unfinished"></translation>
+        <source>Show Delete action (Require restart)</source>
+        <translation>Afficher l&apos;action suppression (Nécessite un redémarrage)</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="114"/>
-        <source>Close to Tray</source>
-        <translation type="unfinished"></translation>
+        <source>D</source>
+        <translation>D</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="115"/>
-        <source>Color schem (Requires restart):</source>
+        <source>Use popup dialogs to create and modify tasks</source>
+        <translation>Utiliser des fenêtres popup pour créer et modifier des tâches</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="116"/>
+        <source>Enable system tray (Require restart)</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="117"/>
+        <source>Hide to tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="118"/>
+        <source>Hide on startupp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="119"/>
+        <source>CheckBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="121"/>
+        <source>Singleton mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="122"/>
+        <source>Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="123"/>
+        <source>System</source>
+        <translation>Système</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="124"/>
+        <source>dark_blue</source>
+        <translation>dark_blue</translation>
+    </message>
+    <message>
+        <location filename="settingsui.py" line="125"/>
+        <source>dark_orange</source>
+        <translation>dark_orange</translation>
     </message>
 </context>
 <context>
     <name>Shortcuts</name>
     <message>
-        <location filename="shortcuts.py" line="24"/>
-        <source>Comment</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="shortcuts.py" line="25"/>
         <source>Shortcut</source>
         <translation>Raccourci</translation>
+    </message>
+    <message>
+        <location filename="shortcuts.py" line="24"/>
+        <source>Comment</source>
+        <translation>Commentaire</translation>
     </message>
 </context>
 <context>
@@ -225,85 +250,85 @@ Exception: {}</source>
 <context>
     <name>TasksListController</name>
     <message>
-        <location filename="tasks_list_controller.py" line="42"/>
+        <location filename="tasks_list_controller.py" line="60"/>
         <source>&amp;Edit Task</source>
         <translation>Editer la tâche</translation>
     </message>
     <message>
-        <location filename="tasks_list_controller.py" line="51"/>
-        <source>&amp;Create New Task</source>
-        <translation>Créer une nouvelle tâche</translation>
-    </message>
-    <message>
-        <location filename="tasks_list_controller.py" line="59"/>
-        <source>&amp;Create a new Task based on a template</source>
-        <translation>Créer une nouvelle tâche à partir d'un modèle</translation>
-    </message>
-    <message>
-        <location filename="tasks_list_controller.py" line="67"/>
-        <source>Copy Selected Tasks</source>
-        <translation>Copier les tâches sélectionnées</translation>
-    </message>
-    <message>
-        <location filename="tasks_list_controller.py" line="75"/>
-        <source>&amp;Delete Selected Tasks</source>
-        <translation>Supprimer les tâches sélectionnées</translation>
-    </message>
-    <message>
-        <location filename="tasks_list_controller.py" line="83"/>
-        <source>C&amp;omplete Selected Tasks</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="tasks_list_controller.py" line="91"/>
-        <source>Decrease Priority</source>
-        <translation>Diminuer la priorité</translation>
-    </message>
-    <message>
-        <location filename="tasks_list_controller.py" line="99"/>
-        <source>Increase Priority</source>
-        <translation>Augmenter la priorité</translation>
-    </message>
-    <message>
-        <location filename="tasks_list_controller.py" line="120"/>
-        <source>Toggle Completeness of</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="tasks_list_controller.py" line="127"/>
+        <location filename="tasks_list_controller.py" line="198"/>
         <source>Delete</source>
         <translation>Supprimer</translation>
     </message>
     <message>
-        <location filename="tasks_list_controller.py" line="134"/>
+        <location filename="tasks_list_controller.py" line="211"/>
+        <source>Confirm</source>
+        <translation>Confirmer</translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="69"/>
+        <source>&amp;Create new task</source>
+        <translation>Créer une nouvelle tâche</translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="77"/>
+        <source>&amp;Create a new task based on current task</source>
+        <translation>Créer une nouvelle tâche basée sur la tâche sélectionnée </translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="85"/>
+        <source>Copy selected tasks</source>
+        <translation>Copier les tâches sélectionnées</translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="93"/>
+        <source>&amp;Delete selected tasks</source>
+        <translation>Supprimer les tâches sélectionnées</translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="101"/>
+        <source>C&amp;omplete selected tasks</source>
+        <translation>Terminer les tâches sélectionnées</translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="109"/>
+        <source>Decrease priority</source>
+        <translation>Réduire la priorité</translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="117"/>
+        <source>Increase priority</source>
+        <translation>Augmenter la priorité</translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="191"/>
+        <source>Toggle completeness of</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tasks_list_controller.py" line="205"/>
         <source>&lt;b&gt;%s the following task?&lt;/b&gt;&lt;ul&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="tasks_list_controller.py" line="136"/>
+        <location filename="tasks_list_controller.py" line="207"/>
         <source>&lt;b&gt;%s the following tasks?&lt;/b&gt;&lt;ul&gt;</source>
         <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="tasks_list_controller.py" line="140"/>
-        <source>Confirm</source>
-        <translation>Confirmer</translation>
     </message>
 </context>
 <context>
     <name>TrayIcon</name>
     <message>
-        <location filename="app.py" line="42"/>
+        <location filename="app.py" line="41"/>
         <source>Create New Task</source>
         <translation>Créer Nouvelle Tâche</translation>
     </message>
     <message>
-        <location filename="app.py" line="45"/>
+        <location filename="app.py" line="44"/>
         <source>Show/Hide Window</source>
         <translation>Monter/Cacher Fenêtre</translation>
     </message>
     <message>
-        <location filename="app.py" line="47"/>
+        <location filename="app.py" line="46"/>
         <source>Exit</source>
         <translation>Quitter</translation>
     </message>
