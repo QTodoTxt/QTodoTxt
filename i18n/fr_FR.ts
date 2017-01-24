c4c478f6405fe35a1b174243cc804baa0c12ad36
Feinte75 <Feinte75@users.noreply.github.com>
2017-01-24 11:38:53 +0100

    Update fr_FR.ts

--- i18n/fr_FR.ts
+++ i18n/fr_FR.ts
@@ -5,42 +5,42 @@
     <message>
         <location filename="main_controller.py" line="70"/>
         <source>Show &amp;Filters</source>
-        <translation type="unfinished"></translation>
+        <translation>Montrer les filtres</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="76"/>
         <source>Show Future &amp;Tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Montrer les tâches à venir</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="82"/>
         <source>Show &amp;Completed Tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Montrer les tâches terminées</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="88"/>
         <source>&amp;Archive Completed Tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Archiver les tâches complétées</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="93"/>
         <source>Show Tool&amp;Bar</source>
-        <translation type="unfinished"></translation>
+        <translation>Montrer la barre d'outils</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="98"/>
         <source>Show Search Bar</source>
-        <translation type="unfinished"></translation>
+        <translation>Montrer la barre de recherche</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="325"/>
         <source>Unsaved changes...</source>
-        <translation type="unfinished"></translation>
+        <translation>Changements non sauvegardés...</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="399"/>
         <source>Revert to saved file (and lose unsaved changes)?</source>
-        <translation type="unfinished"></translation>
+        <translation>Revenir au fichier anterieur (et perdre les changements non sauvegardés)?</translation>
     </message>
     <message>
         <location filename="main_controller.py" line="413"/>
@@ -60,42 +60,42 @@ Exception: {}</source>
     <message>
         <location filename="menu_controller.py" line="26"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="67"/>
         <source>&amp;Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Edition</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="82"/>
         <source>&amp;View</source>
-        <translation type="unfinished"></translation>
+        <translation>Affichage</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="91"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Aide</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="103"/>
         <source>&amp;Shortcuts list</source>
-        <translation type="unfinished"></translation>
+        <translation>Liste des raccourcis</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="119"/>
         <source>&amp;New</source>
-        <translation type="unfinished"></translation>
+        <translation>Nouveau</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="128"/>
         <source>&amp;Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="136"/>
         <source>&amp;Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Sauver</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="144"/>
@@ -105,12 +105,12 @@ Exception: {}</source>
     <message>
         <location filename="menu_controller.py" line="151"/>
         <source>&amp;Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>Préferences</translation>
     </message>
     <message>
         <location filename="menu_controller.py" line="159"/>
         <source>E&amp;xit</source>
-        <translation type="unfinished"></translation>
+        <translation>Quitter</translation>
     </message>
 </context>
 <context>
@@ -118,17 +118,17 @@ Exception: {}</source>
     <message>
         <location filename="settingsui.py" line="100"/>
         <source>Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>Préferences</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="101"/>
         <source>QTodoTxt Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Préferences QTodoTxt</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="102"/>
         <source>Autosave</source>
-        <translation type="unfinished"></translation>
+        <translation>Sauvegarde automatique</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="103"/>
@@ -138,27 +138,27 @@ Exception: {}</source>
     <message>
         <location filename="settingsui.py" line="104"/>
         <source>AutoArchive</source>
-        <translation type="unfinished"></translation>
+        <translation>Archivage automatique</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="105"/>
         <source>Add created date</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajouter date de création</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="106"/>
         <source>Ask for confirmation before task completion</source>
-        <translation type="unfinished"></translation>
+        <translation>Demander confirmation avant de terminer la tâche</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="107"/>
         <source>Lowest task priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Diminuer la priorité de la tâche</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="108"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Fermer</translation>
     </message>
     <message>
         <location filename="settingsui.py" line="110"/>
@@ -201,7 +201,7 @@ Exception: {}</source>
     <message>
         <location filename="shortcuts.py" line="25"/>
         <source>Shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Raccourci</translation>
     </message>
 </context>
 <context>
@@ -209,17 +209,17 @@ Exception: {}</source>
     <message>
         <location filename="shortcutsui.py" line="47"/>
         <source>Shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation>Raccourcis</translation>
     </message>
     <message>
         <location filename="shortcutsui.py" line="48"/>
         <source>Shortcuts list</source>
-        <translation type="unfinished"></translation>
+        <translation>Liste des raccourcis</translation>
     </message>
     <message>
         <location filename="shortcutsui.py" line="49"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Fermer</translation>
     </message>
 </context>
 <context>
@@ -227,27 +227,27 @@ Exception: {}</source>
     <message>
         <location filename="tasks_list_controller.py" line="42"/>
         <source>&amp;Edit Task</source>
-        <translation type="unfinished"></translation>
+        <translation>Editer la tâche</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="51"/>
         <source>&amp;Create New Task</source>
-        <translation type="unfinished"></translation>
+        <translation>Créer une nouvelle tâche</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="59"/>
         <source>&amp;Create a new Task based on a template</source>
-        <translation type="unfinished"></translation>
+        <translation>Créer une nouvelle tâche à partir d'un modèle</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="67"/>
         <source>Copy Selected Tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Copier les tâches sélectionnées</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="75"/>
         <source>&amp;Delete Selected Tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer les tâches sélectionnées</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="83"/>
@@ -257,12 +257,12 @@ Exception: {}</source>
     <message>
         <location filename="tasks_list_controller.py" line="91"/>
         <source>Decrease Priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Diminuer la priorité</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="99"/>
         <source>Increase Priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Augmenter la priorité</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="120"/>
@@ -272,7 +272,7 @@ Exception: {}</source>
     <message>
         <location filename="tasks_list_controller.py" line="127"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer</translation>
     </message>
     <message>
         <location filename="tasks_list_controller.py" line="134"/>
@@ -287,7 +287,7 @@ Exception: {}</source>
     <message>
         <location filename="tasks_list_controller.py" line="140"/>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>Confirmer</translation>
     </message>
 </context>
 <context>
@@ -295,17 +295,17 @@ Exception: {}</source>
     <message>
         <location filename="app.py" line="42"/>
         <source>Create New Task</source>
-        <translation type="unfinished"></translation>
+        <translation>Créer Nouvelle Tâche</translation>
     </message>
     <message>
         <location filename="app.py" line="45"/>
         <source>Show/Hide Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Monter/Cacher Fenêtre</translation>
     </message>
     <message>
         <location filename="app.py" line="47"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Quitter</translation>
     </message>
 </context>
 </TS>
