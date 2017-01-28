94437081ec7504971ada24dd0c6439ff8f21a985
Juraj Harasta <juraj.harasta@gmail.com>
2017-01-28 13:38:13 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (62 of 62 strings)

--- i18n/sk_SK.ts
+++ i18n/sk_SK.ts
@@ -1,58 +1,61 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
+<!DOCTYPE TS>
+<TS version="2.0" language="sk_SK">
 <context>
     <name>MainController</name>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="70"/>
         <source>Show &amp;Filters</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobraziť &amp;filtre</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="76"/>
         <source>Show future &amp;Tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobraziť budúce úlohy</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="82"/>
         <source>Show &amp;Completed tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobraziť &amp;ukončené úlohy</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="88"/>
         <source>&amp;Archive completed tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Archivovať ukončené úlohy</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="93"/>
         <source>Show tool&amp;Bar</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobraziť &amp;panel s nástrojmi</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="98"/>
         <source>Show search bar</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobraziť vyhľadávací panel</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="324"/>
         <source>Unsaved changes...</source>
-        <translation type="unfinished"></translation>
+        <translation>Neuložené zmeny...</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="398"/>
         <source>Revert to saved file (and lose unsaved changes)?</source>
-        <translation type="unfinished"></translation>
+        <translation>Vrátiť k uloženému súboru a stratiť neuložené zmeny?</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="412"/>
-        <source>Current file &apos;{}&apos; is not available.
+        <source>Current file '{}' is not available.
 Exception: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktuálny súbor '{}' nie je dostupný.
+Exception: {}</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/main_controller.py" line="415"/>
         <source>Error opening file: {}.
  Exception:{}</source>
-        <translation type="unfinished"></translation>
+        <translation>Chyba pri otváraní súboru: {}.
+ Exception:{}</translation>
     </message>
 </context>
 <context>
@@ -60,57 +63,57 @@ Exception: {}</source>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="26"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Súbor</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="67"/>
         <source>&amp;Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Editovať</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="82"/>
         <source>&amp;View</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Zobraziť</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="91"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Nápoveda</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="103"/>
         <source>&amp;Shortcuts list</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoznam &amp;skratiek</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="119"/>
         <source>&amp;New</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Nový</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="128"/>
         <source>&amp;Open</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Otvoriť</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="136"/>
         <source>&amp;Save</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Uložiť</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="144"/>
         <source>&amp;Revert</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Vrátiť späť</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="151"/>
         <source>&amp;Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Predvoľby</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/menu_controller.py" line="159"/>
         <source>E&amp;xit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Ukončiť</translation>
     </message>
 </context>
 <context>
@@ -118,102 +121,102 @@ Exception: {}</source>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="106"/>
         <source>SettingsUI</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavenia užívateľského rozhrania</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="107"/>
         <source>QTodoTxt Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavenia QTodoTxt</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="108"/>
         <source>Autosave</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatické uloženie</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="109"/>
         <source>AutoArchive</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatická archivácia</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="110"/>
         <source>Add created date</source>
-        <translation type="unfinished"></translation>
+        <translation>Pridať dátum vytvorenia</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="111"/>
         <source>Ask for confirmation before task completion</source>
-        <translation type="unfinished"></translation>
+        <translation>Opýtať sa na potvrdenie pred dokončením úlohy</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="112"/>
         <source>Show Delete action (Require restart)</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobraz potvrdenie vymazania (nutný reštart)</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="113"/>
         <source>Lowest task priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Najnižšia priorita úlohy</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="114"/>
         <source>D</source>
-        <translation type="unfinished"></translation>
+        <translation>D</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="115"/>
         <source>Use popup dialogs to create and modify tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Používať vyskakovacie okná na vytvorenie a úpravu úloh</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="116"/>
         <source>Enable system tray (Require restart)</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapnúť ikonu na systémovej lište (nutný reštart)</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="117"/>
         <source>Hide to tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Skryť do systémovej lišty</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="118"/>
         <source>Hide on startupp</source>
-        <translation type="unfinished"></translation>
+        <translation>Skryť pri štarte</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="119"/>
         <source>CheckBox</source>
-        <translation type="unfinished"></translation>
+        <translation>CheckBox</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="120"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Zavrieť</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="121"/>
         <source>Singleton mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Mód jednoho okna</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="122"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Téma</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="123"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Systém</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="124"/>
         <source>dark_blue</source>
-        <translation type="unfinished"></translation>
+        <translation>Tmavomodrá</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/settingsui.py" line="125"/>
         <source>dark_orange</source>
-        <translation type="unfinished"></translation>
+        <translation>Tmavooranžová</translation>
     </message>
 </context>
 <context>
@@ -221,12 +224,12 @@ Exception: {}</source>
     <message>
         <location filename="../qtodotxt/ui/dialogs/shortcuts.py" line="24"/>
         <source>Comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Komentár</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/shortcuts.py" line="25"/>
         <source>Shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Skratka</translation>
     </message>
 </context>
 <context>
@@ -234,17 +237,17 @@ Exception: {}</source>
     <message>
         <location filename="../qtodotxt/ui/dialogs/shortcutsui.py" line="47"/>
         <source>Shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation>Skratky</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/shortcutsui.py" line="48"/>
         <source>Shortcuts list</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoznam skratiek</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/dialogs/shortcutsui.py" line="49"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Zavrieť</translation>
     </message>
 </context>
 <context>
@@ -252,67 +255,67 @@ Exception: {}</source>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="60"/>
         <source>&amp;Edit Task</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Editovať úlohu</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="69"/>
         <source>&amp;Create new task</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Vytvoriť novú úlohu</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="77"/>
         <source>&amp;Create a new task based on current task</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Vytvoriť novú úlohu na základe stávajúcej</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="85"/>
         <source>Copy selected tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopírovať vybranú úlohu</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="93"/>
         <source>&amp;Delete selected tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Zmazať vybranú úlohu</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="101"/>
         <source>C&amp;omplete selected tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Dokončiť vybrané úlohy</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="109"/>
         <source>Decrease priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Znížiť prioritu</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="117"/>
         <source>Increase priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Zvýšiť prioritu</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="191"/>
         <source>Toggle completeness of</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokončiť</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="198"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Vymazať</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="205"/>
         <source>&lt;b&gt;%s the following task?&lt;/b&gt;&lt;ul&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%s nasledujúcu úlohu?&lt;/b&gt;&lt;ul&gt;</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="207"/>
         <source>&lt;b&gt;%s the following tasks?&lt;/b&gt;&lt;ul&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%s nasledujúce úlohy?&lt;/b&gt;&lt;ul&gt;</translation>
     </message>
     <message>
         <location filename="../qtodotxt/ui/controllers/tasks_list_controller.py" line="211"/>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>Potvrdiť</translation>
     </message>
 </context>
 <context>
@@ -320,17 +323,17 @@ Exception: {}</source>
     <message>
         <location filename="../qtodotxt/app.py" line="41"/>
         <source>Create New Task</source>
-        <translation type="unfinished"></translation>
+        <translation>Vytvoriť novú úlohu</translation>
     </message>
     <message>
         <location filename="../qtodotxt/app.py" line="44"/>
         <source>Show/Hide Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobraziť/Skryť okno</translation>
     </message>
     <message>
         <location filename="../qtodotxt/app.py" line="46"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukončiť</translation>
     </message>
 </context>
 </TS>
