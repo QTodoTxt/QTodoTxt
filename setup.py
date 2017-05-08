caf5f88aecf3b70f1e9e25f8c60113aedd58684c
oroulet <olivier@loke>
2017-05-08 10:59:30 +0200

    new release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 
 setup(name="qtodotxt", 
-      version="1.8.0alpha1",
+      version="1.9.0",
       description="Cross Platform todo.txt GUI",
       author="QTT Development Team",
       author_email="qtodotxt@googlegroups.com",
